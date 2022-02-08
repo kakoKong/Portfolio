40ee42d04a1b5580d348e2aa568d115e46cfb4de
unknown <khaokong@gmail.com>
2022-02-08 10:51:11 +0000

    add image and hover effects

--- three.js
+++ three.js
@@ -1,7 +1,11 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.132.2/build/three.module.js';
 import { TrackballControls } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/TrackballControls.js'
 
+let lookX = 7;
+let lookY = 0;
+let lookZ = 0;
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xcef2fc);
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const canvas = document.getElementById( "gl-canvas" );
 const renderer = new THREE.WebGL1Renderer({ canvas });
@@ -19,7 +23,7 @@ const light = new THREE.AmbientLight( 0x404040 ); // soft white light scene.add(
 scene.add(light)
 
 const geometry2 = new THREE.PlaneGeometry( 100, 100 );
-const material2 = new THREE.MeshBasicMaterial( {color: 0x4B7591, side: THREE.DoubleSide} );
+const material2 = new THREE.MeshBasicMaterial( {color: 0x264906, side: THREE.DoubleSide} );
 const plane = new THREE.Mesh( geometry2, material2 );
 plane.rotation.x = Math.PI / 2;
 plane.position.y = -5;
@@ -52,6 +56,7 @@ scene.add(skill);
 
 const controls = new TrackballControls(camera, renderer.domElement)
 camera.position.set(8, 0, 0);
+camera.lookAt(0, 10, 0);
 function addBox(){
     const geometry = new THREE.BoxGeometry();
     const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -60,7 +65,7 @@ function addBox(){
 }
 function addReg(){
     const geometry = new THREE.BoxGeometry(50, 10);
-    const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
+    const material = new THREE.MeshPhongMaterial( { color: 0xB2ACAC } );
     const cube = new THREE.Mesh( geometry, material );
     return cube
 }
@@ -72,18 +77,29 @@ function animate() {
     // console.log(document.documentElement.scrollTop)
     // controls.update()
     
-    camera.lookAt(7, 0, 0)
+    camera.lookAt(lookX, lookY, lookZ)
     renderer.render( scene, camera );
-    console.log(camera.position);
+    // console.log(camera.position);
     
 };
 
 animate()
-document.addEventListener( 'mousewheel', (event) => { 
-    camera.position.z += event.deltaY/120;
-    camera.position.x -= event.deltaY/120;
-    console.log(document.body.offsetTop)
-  })
+// document.addEventListener( 'mousewheel', (event) => { 
+//     camera.position.z += event.deltaY/120;
+//     camera.position.x -= event.deltaY/120;
+//     console.log(document.body.offsetTop)
+//   })
+
+function updateCamera(ev) {
+    // let div1 = document.getElementById("div1");
+    lookX = 10 - window.scrollY / 8.0;
+    lookZ = 10 - window.scrollY / 8;
+    console.log(window.scrollY)
+    
+    // camera.position.z = 10 - window.scrollY / 80.0;
+}
+
+window.addEventListener("scroll", updateCamera);
 // // Renderer
 // const canvas = document.getElementById( "gl-canvas" );
 // renderer = new THREE.WebGL1Renderer({ canvas });
